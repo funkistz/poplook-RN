fce8c3864eb664c433193016affe85e55c1da7ed
Hannis <hannis@poplook.com>
2023-04-13 12:40:46 +0800

    Add module

--- types/classnames/index.ts
+++ types/classnames/index.ts
@@ -4,6 +4,7 @@ declare module 'react-native-insider';
 declare module 'react-native-insider/src/InsiderCallbackType';
 declare module 'react-native-searchable-dropdown';
 declare module 'react-native-ipay88-integration';
+declare module 'react-native-eghl';
 declare module 'react-native-vector-icons/Ionicons';
 declare module '@env' {
     export const API_URL: string;
