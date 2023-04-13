4f74770e4bc47254930a97db08541d93eafbeed2
Hannis <hannis@poplook.com>
2023-04-13 12:39:03 +0800

    Add Payment Service

--- Services/PaymentService.tsx
+++ Services/PaymentService.tsx
@@ -12,8 +12,25 @@ const PaymentService = {
 
         return APIService.postMethod('PaymentProcessor/redirect_atomepayment', params);
 
+    },
+
+    async getPaymentInfo(refId: String) {
+
+        let base64 = require("base-64"); 
+
+        // const options = {
+        //   url: 'https://api.apaylater.net/v2/payments/' + refId,
+        //   headers: { "Authorization": "Basic " + base64.encode('OOCBRiiB2XjBAw1kQVI6IabtnOcYcE' + ":" + 'bDcyYGGNyir9anCC0mjCzCXIoDft5S') },
+        //   method: 'GET'
+        // };
+        
+        return fetch('https://api.apaylater.net/v2/payments/' + refId, { 
+            method: 'GET', 
+            headers: new Headers({ "Authorization": "Basic " + base64.encode('fb9f8e94420c4dea8781d9282dbdc9e3' + ":" + '587c1346e7224210898b22f9d6e539cd') }),
+        });;
     }
 
 }
 
 export default PaymentService;
+
