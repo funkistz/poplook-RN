a8a09c35ae799b5fb93d2a06d16084ee4e80c45e
Hannis <hannis@poplook.com>
2023-06-07 18:27:30 +0800

    Update size

--- components/Products/SizeList.tsx
+++ components/Products/SizeList.tsx
@@ -1,22 +1,24 @@
 import { Button, HStack, Spacer, Text, Flex } from "native-base";
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Platform } from 'react-native';
+import React, { useEffect } from 'react';
 
 const win = Dimensions.get('window');
 
 function SizeList({ attribute, setSizeSelected, sizeSelected }: any) {
 
+
     const getTextColor = (id_product_attribute: any) => {
 
         return (sizeSelected != id_product_attribute) ? '#000' : '#fff';
     }
-
+     
     return (<>
         <Flex direction="row" flex={1} flexWrap="wrap" justifyContent="flex-start">
             {attribute && attribute.length > 0 && attribute.map((res: any, index: any) => {
                 return <Button onPress={() => setSizeSelected(Number(res.id_product_attribute))} key={index} style={styles.chip} variant="outline" size='sm'
                     backgroundColor={(sizeSelected == res.id_product_attribute) ? '#000' : '#fff'}
                     borderColor={(sizeSelected == res.id_product_attribute) ? '#000' : '#ccc'}
-                    width={(res.attribute_name.includes("Year") ? (win.width / 5) + 2: (win.width/ 6 - 2.5))}
+                    width={Platform.OS == "ios" ? res.attribute_name.includes("Year") ? (win.width / 5) : (win.width/ 6 - 2.5) :  res.attribute_name.includes("Year") ? (win.width / 5) + 2: (win.width/ 6 - 2.5)}
                     _text={{ color: getTextColor(res.id_product_attribute), fontSize: 13 }}
                     isDisabled={res.quantity == 0 ? true : false}
                     >
