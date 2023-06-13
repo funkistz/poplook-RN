c081152483f9bb0e703e524e4768a066351ff26c
Hannis <hannis@poplook.com>
2023-06-13 11:51:11 +0800

    Add visit our store api

--- Services/BannerService.tsx
+++ Services/BannerService.tsx
@@ -6,6 +6,12 @@ const BannerService = {
 
         return APIService.getMethod('banners/mobilev2');
 
+    },
+
+    async getStores() {
+
+        return APIService.getMethod('banners/visit_our_store/visit_our_store_mobile');
+
     }
 }
 
