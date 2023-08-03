3b396626f956c9fca474dc43efd3d29265ead0c0
zariff91 <73475512+zariff91@users.noreply.github.com>
2023-08-03 11:16:42 +0800

    Update redux cart

--- Redux/Slices/Cart.tsx
+++ Redux/Slices/Cart.tsx
@@ -1,10 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import AuthService from '../../Services/AuthService'
 import CartService from '../../Services/CartService'
 import GeneralService from '../../Services/GeneralService'
-import { useDispatch } from 'react-redux'
-// import { assignUser } from './Sessions'
 
 export interface CartState {
     id_cart: Number | null,
@@ -27,8 +23,7 @@ export const getCart: any = createAsyncThunk(
             const state: any = getState();
             const id_cart = state.cart.id_cart;
             const response = await CartService.getCart(id_cart);
-            let data = await response.json()
-            console.log("datacart", data)
+            let data = await response.data
 
             if (response.status == 200) {
                 if (data.code == 200) {
@@ -40,7 +35,6 @@ export const getCart: any = createAsyncThunk(
                 return rejectWithValue(data)
             }
         } catch (e: any) {
-            console.log("Error", e.response.data)
             rejectWithValue(e.response.data)
         }
     }
@@ -53,23 +47,19 @@ export const addToCart: any = createAsyncThunk(
             const state: any = getState();
             const id_cart = state.cart.id_cart;
             const id_customer = state.session.user ? state.session.user.id_customer : null;
+            const id_shop = state.session.country.id_shop;
 
-            console.log("state before", state)
-
-            let params: any = { id_cart, id_product, id_product_attribute, quantity };
+            let params: any = { id_cart, id_product, id_product_attribute, quantity, id_shop };
 
             if (id_customer) {
                 params.id_customer = id_customer;
             }
 
-            const response = await CartService.addToCart(params);
-            let data = await response.json()
-            console.log("responsecart", data)
+            const response = await CartService.addToCart(id_cart, params);
+            let data = await response.data
 
             if (response.status == 201) {
                 if (data.code == 201) {
-                    // dispatch(assignUser(data.data.id_cart))
-                    // dispatch(getCart())
                     return data
                 } else {
                     return rejectWithValue(data)
@@ -78,7 +68,6 @@ export const addToCart: any = createAsyncThunk(
                 return rejectWithValue(data)
             }
         } catch (e: any) {
-            console.log("Error", e.response.data)
             rejectWithValue(e.response.data)
         }
     }
@@ -91,12 +80,8 @@ export const delToCart: any = createAsyncThunk(
             const state: any = getState();
             const id_cart = state.cart.id_cart;
 
-            console.log('id_product: ', id_product)
-
-            const response = await CartService.delToCart({ id_cart, id_product, id_product_attribute });
-            let data = await response.json()
-            console.log("response", response)
-            console.log("data", data)
+            const response = await CartService.delToCart(id_cart, id_product, id_product_attribute);
+            let data = await response.data
 
             if (response.status == 201) {
                 if (data.code == 201) {
@@ -109,7 +94,6 @@ export const delToCart: any = createAsyncThunk(
                 return rejectWithValue(data)
             }
         } catch (e: any) {
-            console.log("Error", e.response.data)
             rejectWithValue(e.response.data)
         }
     }
@@ -137,15 +121,16 @@ export const cartSlice = createSlice({
             temp.total_item = 0;
 
             state = { ...state, ...temp }
-            console.log('clearstateresult', state)
             return state;
         },
     },
     extraReducers: (builder) => {
         builder.addCase(addToCart.fulfilled, (state, { payload }) => {
+
             GeneralService.toast({ description: payload.message });
             const temp: any = {};
             if (payload.data) {
+
                 temp.id_cart = payload.data.id_cart;
                 temp.total_item = payload.data.totalItemInCart;
                 temp.cartLoading = false;
@@ -155,6 +140,7 @@ export const cartSlice = createSlice({
             return state;
         })
             .addCase(addToCart.pending, (state, { payload }) => {
+
                 const temp: any = {};
                 temp.cartLoading = true;
 
@@ -163,6 +149,7 @@ export const cartSlice = createSlice({
                 return state;
             })
             .addCase(addToCart.rejected, (state, { payload }) => {
+
                 GeneralService.toast({ description: payload.message });
                 const temp: any = {};
                 temp.cartLoading = false;
@@ -175,7 +162,9 @@ export const cartSlice = createSlice({
             .addCase(getCart.fulfilled, (state, { payload }) => {
 
                 const temp: any = {};
+
                 if (payload.data) {
+
                     temp.id_cart = payload.data.id_cart;
                     temp.data = payload.data;
                     temp.total_item = payload.data.totalItemInCart;
@@ -216,7 +205,6 @@ export const cartSlice = createSlice({
     },
 })
 
-// Action creators are generated for each case reducer function
 export const { clearCart, assignCartId } = cartSlice.actions
 
 export const cartSelector = (state: any) => state.cart
