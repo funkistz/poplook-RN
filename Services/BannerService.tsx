c761862aea1db99bd7c246b201b50581a97cb299
Hannis <hannis@poplook.com>
2023-08-07 12:43:58 +0800

    Add video service

--- Services/BannerService.tsx
+++ Services/BannerService.tsx
@@ -8,6 +8,12 @@ const BannerService = {
 
     },
 
+    async getBannersVideo() {
+
+        return APIService.getMethod('banners/video');
+
+    },
+
     async getStores() {
 
         return APIService.getMethod('banners/visit_our_store/visit_our_store_mobile');
