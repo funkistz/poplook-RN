1845c8fa0b0b5555a026e5f9c4510d16218a2954
Hannis <hannis@poplook.com>
2023-08-08 17:39:27 +0800

    Fix device type

--- Services/AxiosService.tsx
+++ Services/AxiosService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { store } from '../Redux/app';
-import { X_API_KEY } from "@env"
+import { IOS_VERSION, X_API_KEY } from "@env"
 
 const api = axios.create({
   baseURL: 'https://api.poplook.com/api/',
@@ -12,10 +12,17 @@ api.interceptors.request.use(
     const state: any = store.getState();
     const token = state.session.token; 
     const id_customer = state.session.user ? state.session.user.id_customer : '' ;
+    const device_type = state.session ? state.session.device_type : 'android' ;
+    const id_shop = state.session.country ? state.session.country.id_shop : '1';
 
     const params = new URLSearchParams();
 
     params.append('id_customer', id_customer);
+    params.append('device_type', device_type);
+    params.append('api_version', 'apps');
+    params.append('app_version', IOS_VERSION);
+    params.append('id_lang', '1');
+    params.append('id_shop', id_shop);
 
     config.url += `?${params.toString()}`;
 
