ab2841f6b1ee950179bb47c4a9ae96bfa499b1df
Hannis <hannis@poplook.com>
2023-11-02 14:48:49 +0800

    Carousel

--- pages/BannerPage.tsx
+++ pages/BannerPage.tsx
@@ -18,10 +18,15 @@ import Video from 'react-native-video';
 import { WINDOW_HEIGHT } from '@gorhom/bottom-sheet';
 import Slider2 from '../components/Slider2';
 import AutoImage from '../components/AutoImage';
-import Carousel from 'react-native-snap-carousel';
 import MyImageCarousel from '../components/Carousel2';
+import Carousel2, { Pagination } from 'react-native-snap-carousel';
+import { GestureHandlerRootView, PanGestureHandler, State } from 'react-native-gesture-handler';
+import Animated, { useSharedValue, withSpring, withTiming, useAnimatedStyle } from 'react-native-reanimated';
+import Carousel from 'react-native-reanimated-carousel';
+import PaginationDot from 'react-native-insta-pagination-dots'
 
 const win = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 export default function BannerPage({ navigation }: { navigation: any }) {
 
@@ -79,6 +84,63 @@ export default function BannerPage({ navigation }: { navigation: any }) {
         );
     };
 
+    const renderItem2 = ({item, index} : any) => {
+        
+        return (
+            <View> 
+                {/* <TouchableOpacity key={index} onPress={() => goToCategory(item)}> */}
+                    <Image source={{ uri: 'https://api.poplook.com/' + item.href }} alt="image" style={{ width: win.width, height: 250 }}/>
+                {/* </TouchableOpacity> */}
+            </View>
+        );
+    };
+
+    const [activeSlide, setActiveSlide] = useState(0);
+    const translateX = useSharedValue(0);
+
+    const handlePrev = () => {
+        if (currentImageIndex > 0) {
+        setCurrentImageIndex(currentImageIndex - 1);
+        translateX.value = withSpring(-currentImageIndex * width);
+        }
+    };
+
+    const handleNext = (data: any) => {
+        if (currentImageIndex < data.length - 1) {
+        setCurrentImageIndex(currentImageIndex + 1);
+        translateX.value = withSpring(-(currentImageIndex + 1) * width);
+        }
+    };
+
+    const renderCarouselItem = ({ item, index } : any) => (<>
+        <Text color="black">kai</Text>
+        <View style={styles.carouselItem}>
+        <Image source={{ uri: 'https://api.poplook.com/' + item.href }} alt="image" style={{ width: win.width, height: 250 }}/>
+        </View>
+        </>
+    );
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        transform: [{ translateX: translateX.value }],
+    }));
+
+    const [currentImageIndex, setCurrentImageIndex] = useState<any>(0);
+
+    const navigateTo = direction => {
+        if (direction === 'next' && index.current < images.length - 1) {
+          index.current++;
+        } else if (direction === 'prev' && index.current > 0) {
+          index.current--;
+        }
+    
+        timing(translateX, {
+          toValue: -index.current * width,
+          duration: 300,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }).start();
+    };
+
     return (
         <ScrollView>
             {banners.map((data: any, index: any) => {
@@ -107,8 +169,9 @@ export default function BannerPage({ navigation }: { navigation: any }) {
                         }
 
                         {item.block.type == 'slider' && 
-                            <View>
-                                <Carousel
+                            <View style={styles.container}>
+                                <Text color="black">{item.block.type}</Text>
+                                <Carousel2
                                     layout={'default'}
                                     ref={(ref: any) => setCarouselRef(ref)}
                                     data={item.block.resource}
@@ -127,7 +190,58 @@ export default function BannerPage({ navigation }: { navigation: any }) {
 
                         {item.block.type == 'carousel' && 
                             <View style={styles.container}>
-                                <MyImageCarousel />
+                                <Text color="black">{item.block.type}</Text>
+                                <Carousel
+                                    width={win.width}
+                                    height={200}
+                                    loop={true}
+                                    data={item.block.resource}
+                                    renderItem={renderItem2}
+                                    onSnapToItem={(index) => {
+                                        setCurrentImageIndex(index)
+                                    }}
+                                />
+
+                            {/* <PaginationDot
+                                activeDotColor={'black'}
+                                curPage={currentImageIndex}
+                                maxPage={item.block.resource.length}
+                            /> */}
+
+                            <View style={styles.controls}>
+                                <TouchableOpacity onPress={handlePrev} style={styles.controlButton}>
+                                <Text>Prev</Text>
+                                </TouchableOpacity>
+                                    <Pagination
+                                    dotsLength={item.block.resource.length}
+                                    activeDotIndex={currentImageIndex}
+                                    containerStyle={styles.paginationContainer}
+                                    dotStyle={styles.paginationDot}
+                                    inactiveDotOpacity={0.6}
+                                    inactiveDotScale={0.8}
+                                    />
+
+                                {/* <PaginationDot
+                                    activeDotColor={'black'}
+                                    curPage={currentImageIndex}
+                                    maxPage={item.block.resource.length}
+                                />      */}
+
+                                <TouchableOpacity onPress={() => handleNext(item.block.resource)} style={styles.controlButton}>
+                                <Text>Next</Text>
+                                </TouchableOpacity>
+                            </View>
+
+                                {/* <Pagination
+                                    dotsLength={item.block.resource.length}
+                                    activeDotIndex={currentImageIndex}
+                                    containerStyle={styles.paginationContainer}
+                                    dotStyle={styles.dot}
+                                    inactiveDotStyle={styles.inactiveDot}
+                                    inactiveDotOpacity={0.6}
+                                    inactiveDotScale={0.8}
+                                    // inactiveDotScale={}
+                                /> */}
                             </View>
                         }
 
@@ -165,5 +279,58 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         // backgroundColor: 'white',
+    },
+    carouselContainer: {
+        width: width * 0.9,
+        height: 200,
+      },
+      carousel: {
+        flex: 1,
+      },
+      carouselItem: {
+        width: width,
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgray',
+    },
+    image2: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+      controls: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    controlButton: {
+        padding: 10,
+        backgroundColor: 'lightblue',
+        marginHorizontal: 10,
+    },
+    // paginationContainer: {
+    //     paddingVertical: 10,
+    // },
+    paginationDot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        marginHorizontal: 2,
+        backgroundColor: 'blue',
+    },
+    paginationContainer: {
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: 10,
+    },
+    dot: {
+        width: 8,
+        height: 8,
+        borderRadius: 4,
+        backgroundColor: 'rgba(0, 0, 0, 2.0)'
+    },
+    inactiveDot: {
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
     },
 });
