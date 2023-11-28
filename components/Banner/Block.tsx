75c5ab445e37ddd1ff0493557b4492bca54f3632
Hannis <hannis@poplook.com>
2023-11-28 17:04:08 +0800

    Update block

--- components/Banner/Block.tsx
+++ components/Banner/Block.tsx
@@ -1,6 +1,7 @@
-import { Dimensions, View } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 import { Text, FlatList, Center } from 'native-base';
 import React, { memo } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import Images from './Image';
 import Videos from './Video';
 
@@ -8,15 +9,32 @@ const win = Dimensions.get('window');
 
 const Blocks = memo(function Greeting({ item, height }: any) {
 
+    const navigation: any = useNavigation();
+
+    const goToCategory = (item: any) => {
+
+        const params = {
+            category_id: String(item.block.resource.categoryId),
+            category_name: item.name
+        };
+
+        console.log('title', item.block.resource.categoryId);
+
+        navigation.navigate('Home', { screen: 'CategoryPage', params: params, title: String(item.block.resource.categoryId) });
+
+    }
+
     return (
         <Center>
-            {item.block.resource.type == 'image' && 
-                <Images data={item.block.resource} width={win.width} height={height}></Images>
-            }
+            <TouchableOpacity onPress={() => goToCategory(item)}>
+                {item.block.resource.type == 'image' && 
+                    <Images data={item.block.resource} width={win.width} height={height}></Images>
+                }
 
-            {item.block.resource.type == 'video' && 
-                <Videos data={item.block.resource} width={win.width} height={height}></Videos>
-            }
+                {item.block.resource.type == 'video' && 
+                    <Videos data={item.block.resource} width={win.width} height={height}></Videos>
+                }
+            </TouchableOpacity>
         </Center>
     );
 })
