525861fab9f290e66d682848cceffb62a5580872
Hannis <hannis@poplook.com>
2023-12-13 11:40:15 +0800

    Fix slider

--- components/Banner/Sliders.tsx
+++ components/Banner/Sliders.tsx
@@ -1,51 +1,56 @@
 import { StyleSheet, View, Dimensions, TouchableOpacity, useWindowDimensions } from 'react-native';
-import { ScrollView, HStack, VStack, Center, Text } from 'native-base';
 import React, { useState, memo } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Carousel from 'react-native-snap-carousel';
 import Images from './Image';
 import Videos from './Video';
-import Texts from './TextWithStyle';
 import TextWithStyle from './TextWithStyle';
 
 const win = Dimensions.get('window');
 
-const Sliders = memo(function Greeting({ item, child, height }: any) {
+const { width: screenWidth } = Dimensions.get('window');
+
+const Sliders = memo(function Greeting({ item }: any) {
 
     const navigation: any = useNavigation();
-    const layout = useWindowDimensions();
+
+    const slideGap = item.block.slideGap/100;
+    const slideSize = item.block.slideSize;
+    const length = item.block.resource.length;
+    const itemWidth = (screenWidth * slideSize) / 100;
+
     const [carouselRef, setCarouselRef] = useState(null);
 
     const goToCategory = (item: any) => {
-      
+
         const params = {
-            category_id: String(item.categoryId),
-            category_name: ''
+            category_id: String(item.linkData.id),
+            category_name: item.linkData.name
         };
 
-
-        navigation.navigate('Home', { screen: 'CategoryPage', params: params, title: String(item.categoryId) });
+        navigation.navigate('Home', { screen: 'CategoryPage', params: params, title: String(item.linkData.id) });
 
     }
 
     const renderItem = ({item, index} : any) => {
-        
         return (
-            <TouchableOpacity onPress={() => goToCategory(item)} key={index}>
-                <VStack key={index}>
-                    <Center w={layout.width / 3} bg="grey" borderRadius={10} shadow={1}>
-                        {item.type == 'image' && 
-                            <Images width={layout.width /3} data={item} height={height}></Images>
-                        }
-                        
-                        {item.type == 'video' && 
-                            <Videos width={layout.width /3} height={height} data={item}></Videos>
-                        }
-                    </Center>
-                </VStack>
-                <TextWithStyle data={item.labelObj}></TextWithStyle>
-            </TouchableOpacity>
-        );
+            <>
+            <View style={styles.carouselItem}>
+                <TouchableOpacity onPress={() => goToCategory(item)} key={index}> 
+                
+                    {item.type == 'image' && 
+                        <Images data={item} column={100/slideSize}></Images>
+                    }
+                    
+                    {item.type == 'video' && 
+                        <Videos data={item}></Videos>
+                    }
+                        <TextWithStyle data={item.labelObj}></TextWithStyle>
+
+                </TouchableOpacity>
+            </View>
+            </>
+        )
     };
 
     return (
@@ -53,26 +58,22 @@ const Sliders = memo(function Greeting({ item, child, height }: any) {
             <View style={styles.container}>
 
                 <TextWithStyle data={item.block.labelObj}></TextWithStyle>
-                 
-                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                    <HStack>
-                        <Carousel
-                            layout={'default'}
-                            ref={(ref: any) => setCarouselRef(ref)}
-                            data={item.block.resource}
-                            renderItem={renderItem}
-                            sliderWidth={win.width}
-                            itemWidth={win.width * (item.block.slideSize / 100) + item.block.slideGap}
-                            activeSlideAlignment="start"
-                            inactiveSlideScale={1}
-                            inactiveSlideOpacity={1}
-                            onSnapToItem={(index: any) => {
-                            }}
-                            removeClippedSubviews={false}
-                            contentContainerCustomStyle={{overflow: 'hidden', width: (win.width * (item.block.slideSize / 100) + item.block.slideGap + 3) * (item.block.resource.length)}}
-                        />
-                    </HStack>
-                </ScrollView>
+
+                <Carousel
+                    slideStyle={{ }}
+                    layout={'default'}
+                    ref={(ref: any) => setCarouselRef(ref)}
+                    data={item.block.resource}
+                    renderItem={renderItem}
+                    sliderWidth={win.width}
+                    itemWidth={itemWidth + slideGap} 
+                    // inactiveSlideScale={1}
+                    inactiveSlideOpacity={1}
+                    activeSlideAlignment="start"
+                    onSnapToItem={(index: any) => {
+                    }}
+                    removeClippedSubviews={false}
+                />
             </View>
         </>
     );
@@ -81,12 +82,15 @@ const Sliders = memo(function Greeting({ item, child, height }: any) {
 export default Sliders;
 
 const styles = StyleSheet.create({
-    image: {
-        borderRadius: 6,
-    },
     container: {
         flex: 1,
         alignItems: 'flex-start',
         justifyContent: 'flex-start',
     },
+    carouselItem: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',// Adjust as needed for space gap
+        marginHorizontal: 10
+      }
 })
