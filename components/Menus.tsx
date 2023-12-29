df3a9115f921ca5157439d6895b9561845d59f9a
Hannis <hannis@poplook.com>
2023-12-29 10:30:02 +0800

    Update menu

--- components/Menus.tsx
+++ components/Menus.tsx
@@ -5,6 +5,8 @@ import { Center, ScrollView, Flex, Text } from 'native-base';
 import Tabs from './Tabs';
 import Sliders from './Banner/Sliders';
 import Blocks from './Banner/Block';
+import Grid from './Banner/Grid';
+import Carousels from './Banner/Carousel';
 
 const win = Dimensions.get('window');
 
@@ -35,31 +37,81 @@ const Menus = memo(function Greeting({ categories }: { categories: any }) {
 
         category.block.map(async (data: any, index: any) => {
 
-            <Center key={index}>
-
                 {data.children.map(async (item: any, index: any) => {
 
                     if (item.block.type == 'block') {
                         tabs.push(
-                            <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
-                                height: item.height, width: getChildWidth(item.col) }} key={index}>
-                                <Blocks item={item} type={'menu'}></Blocks>
+                            <Flex style={{ flexDirection: data.flex.direction, flexWrap: data.flex.wrap, justifyContent: data.flex.justifyContent,
+                                paddingTop: data.padding.top, paddingRight: data.padding.right, paddingBottom: data.padding.bottom, paddingLeft: data.padding.left, 
+                                height: data.height, width: '100%', backgroundColor: data.backgroundColor }} 
+                                key={index}>
+                                <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
+                                    height: item.height, width: getChildWidth(item.col) }} key={index}>
+                                    <Blocks item={item} type={'menu'}></Blocks>
+                                </Flex>
                             </Flex>
                         );
                     }
 
-                    if (item.block.type == 'slider') {
+                    else if (item.block.type == 'slider') {
                         tabs.push(
-                            <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
-                                height: item.height, width: getChildWidth(item.col) }} key={index}>
-                                <Sliders item={item} type={'menu'}></Sliders>
+                            <Flex style={{ flexDirection: data.flex.direction, flexWrap: data.flex.wrap, justifyContent: data.flex.justifyContent,
+                                paddingTop: data.padding.top, paddingRight: data.padding.right, paddingBottom: data.padding.bottom, paddingLeft: data.padding.left, 
+                                height: data.height, width: '100%', backgroundColor: data.backgroundColor }} 
+                                key={index}>
+                                <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
+                                    height: item.height, width: getChildWidth(item.col) }} key={index}>
+                                    <Sliders item={item} type={'menu'}></Sliders>
+                                </Flex>
+                            </Flex>
+                        );
+                    }
+
+                    else if (item.block.type == 'grid') {
+                        tabs.push(
+                            <Flex style={{ flexDirection: data.flex.direction, flexWrap: data.flex.wrap, justifyContent: data.flex.justifyContent,
+                                paddingTop: data.padding.top, paddingRight: data.padding.right, paddingBottom: data.padding.bottom, paddingLeft: data.padding.left, 
+                                height: data.height, width: '100%', backgroundColor: data.backgroundColor }} 
+                                key={index}>
+                                <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
+                                    height: item.height, width: getChildWidth(item.col) }} key={index}>
+                                    <Grid item={item} type={'menu'}></Grid>  
+                                </Flex>
+                            </Flex>
+                        );
+                    }
+
+                    else if (item.block.type == 'product_list') {
+                        tabs.push(
+                            <Flex style={{ flexDirection: data.flex.direction, flexWrap: data.flex.wrap, justifyContent: data.flex.justifyContent,
+                                paddingTop: data.padding.top, paddingRight: data.padding.right, paddingBottom: data.padding.bottom, paddingLeft: data.padding.left, 
+                                height: data.height, width: '100%', backgroundColor: data.backgroundColor }} 
+                                key={index}>
+                                <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
+                                    height: item.height, width: getChildWidth(item.col) }} key={index}>
+                                    <Sliders item={item} type={'menu'}></Sliders>
+                                </Flex>
+                            </Flex>
+                        );
+                    }
+
+                    else if (item.block.type == 'carousel') {
+                        tabs.push(
+                            <Flex style={{ flexDirection: data.flex.direction, flexWrap: data.flex.wrap, justifyContent: data.flex.justifyContent,
+                                paddingTop: data.padding.top, paddingRight: data.padding.right, paddingBottom: data.padding.bottom, paddingLeft: data.padding.left, 
+                                height: data.height, width: '100%', backgroundColor: data.backgroundColor }} 
+                                key={index}>
+                                <Flex style={{ backgroundColor: item.backgroundColor, paddingTop: item.padding.top, paddingRight: item.padding.right, paddingBottom: item.padding.bottom, paddingLeft: item.padding.left, 
+                                    height: item.height, width: getChildWidth(item.col) }} key={index}>
+                                    <Carousels item={item} type={'menu'}></Carousels> 
+                                </Flex>
                             </Flex>
                         );
                     }
 
                 })}
 
-            </Center>
+            // </Flex>
 
         })
 
